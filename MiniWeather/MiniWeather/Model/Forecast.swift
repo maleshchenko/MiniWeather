3c845050a883759958461122cd2633426bd52764
Mykola Aleshchenko <maleschenko@homeawaycorp.com>
2019-08-02 20:12:10 +0300

    Update .gitignore

---
+++ MiniWeather/MiniWeather/Model/Forecast.swift
@@ -0,0 +1,9 @@
+//
+//  Forecast.swift
+//  MiniWeather
+//
+//  Created by Mykola Aleschenko on 8/2/19.
+//  Copyright © 2019 Mykola Aleschenko. All rights reserved.
+//
+
+import Foundation
---
+++ MiniWeather/MiniWeather/Model/WeatherInfo.swift
@@ -0,0 +1,13 @@
+//
+//  Forecast.swift
+//  MiniWeather
+//
+//  Created by Mykola Aleschenko on 8/2/19.
+//  Copyright © 2019 Mykola Aleschenko. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherInfo: Decodable {
+    let currentForecast: Forecast
+}
