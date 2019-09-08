4a08dc9f56b2f2ddb6a6ded07e36e608e34bcd5b
Mykola Aleshchenko <maleschenko@homeawaycorp.com>
2019-09-09 01:18:55 +0300

    Add simplistic unit test for model loading/parsing/initialization

--- MiniWeather/MiniWeatherTests/MiniWeatherTests.swift
+++ MiniWeather/MiniWeatherTests/MiniWeatherTests.swift
@@ -11,24 +11,24 @@ import XCTest
 
 class MiniWeatherTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testModelInit() {
+        if let path = Bundle.main.path(forResource: "testData", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                print(jsonResult)
+                if let jsonResult = jsonResult as? [String: AnyObject], let currently = jsonResult["currently"] as? Double {
+                    let forecast = Forecast(temperature: currently)
+                    let weatherInfo = WeatherInfo(currently: forecast)
+                    
+                    XCTAssertNotNil(forecast)
+                    XCTAssertNotNil(weatherInfo)
+                }
+            } catch {
+                XCTFail("failed to parse json file")
+            }
+        } else {
+            XCTFail("failed to load json file")
         }
     }
-
 }
--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Minimalist weather app for iOS
 
 TODO:
 
-- add unit tests
 - add WatchOS support
 - use recent data instead of querying API all the time
 
